bcce818d6fead8011683866abd7b9b073ab2c196
Kristina136 <criska.2010@yandex.ru>
2023-07-12 14:10:34 +0200

    new

---
+++ src/OneRecipe.js
@@ -0,0 +1,46 @@
+function OneRecipe ({list, newFunc, newFunc1, setList, newFunk1, label, calories, ingredientLines, url, img, ingredients}){
+
+    let ArrFromAlIng=""
+    let filteredIng=[]
+    let filteredArr=[]
+
+
+
+
+     const filtered=(ing, id)=>{
+     filteredArr.push(filteredIng.filter((item, index)=>(index === id)))
+    newFunc(list.push(<li className="list" key={id} onClick={line} >{filteredArr}</li>))
+   }
+const line =(e)=>{
+    const li= e.target
+    li.classList.toggle("red")
+    if(e.detail === 2){
+        li.classList.toggle("del")
+    }
+}
+
+
+      return(
+      <div>
+      <div className="eachRecipe">
+        <img src={img} alt="alt" width="200px"/>
+<h1>{label}</h1>
+<h2>Callories : {calories.toFixed()} Kcal</h2>
+<ul>
+    {ingredients.map((ing,id)=>{
+         let a= (ArrFromAlIng+=ing.food+"+").split("+")
+         if (a.length===ingredients.length+1){filteredIng.push(...a)} 
+          return(<li className="allIng" onClick={()=>filtered(ing, id)} key={id}>{ing.food}</li>)
+        }
+    )}
+</ul>
+
+<a href={url}>Open recipe</a>
+
+
+
+</div>
+</div>
+    )
+}
+export default OneRecipe;
