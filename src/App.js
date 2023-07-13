6c26328f3b2acb02315deaae07f75b3502acea75
Kristina136 <criska.2010@yandex.ru>
2023-07-13 17:53:12 +0200

    qwestion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import './App.css';
 import OneRecipe from './OneRecipe';
@@ -6,31 +5,43 @@ import Added from './Added';
 
 
 
-
 function App() {
-  
+
+
   const id="640ffb22";
   const key= "4229630f925fe6fd8eb1406c7dff251b";
 
   const[input, setInput]= useState("");
   const[myRec, setMyRes]= useState([]);
-  const[wordApi, setWordApi]= useState("salmon");
-
- const [list, setList]=useState([])
-//   
-
- const filtered=()=>{
- setList([list])
-  }
-
-  const deleted=()=>{
-    setList([list])
-     }
+  const[wordApi, setWordApi]= useState(localStorage.wordApi || "salmon");
+  
+  const [list, setList]=useState([])
+  const [word, setWord] =useState([])
 
 
 
+
+ // localStorage for save a word in input
   useEffect(()=>{
-    
+    localStorage.setItem("wordApi", wordApi)
+  }, [wordApi])
+
+  //  // localStorage for save a recipe by click
+  // useEffect(()=>{
+  //   localStorage.setItem("word", word)
+  // }, [word])
+ 
+const filtered=()=>{
+  setList([list])
+  }
+const save=()=>{
+  setWord([word])
+}
+
+
+
+//get API
+  useEffect(()=>{
     const getRecipe = async() =>{
       const response= await fetch(`https://api.edamam.com/search?q=${wordApi}&app_id=${id}&app_key=${key}`)
       const data=await response.json();
@@ -40,30 +51,35 @@ function App() {
       },[wordApi] )
 
 
-     
+     //look what write user
   const userInput=(e)=>{
        setInput(e.target.value)
        }
+
+       //change state with new word by click buttom or enter
   const searchRec =(e)=>{
-    e.preventDefault()
+   e.preventDefault()
         setWordApi(input);
        }
-
-
-
+ 
+  
 
   return (
     <div className="App">
-    
      <h1>Find recipe by ingredient</h1>
     <Added 
     list={list}/>
-   
      <form onSubmit={searchRec}>
 <input onChange={userInput} value={input} placeholder="Find recipes..."/>
 </form>
 <button onClick={searchRec}>Search</button>
   <div>
+    <h2> {myRec.length} recipes is found</h2>
+<div className='containerSaved'>
+  {word}
+  </div>
+
+
 {myRec.map((el,i)=>{return<OneRecipe key={i}
  label={el.recipe.label}
  calories={el.recipe.calories}
@@ -72,10 +88,10 @@ function App() {
  img={el.recipe.image}
 ingredients= {el.recipe.ingredients}
 newFunc={filtered}
-newFunk1={deleted}
+newFunk1={save}
 list={list}
-setList={setList}
-
+word={word}
+setInput={setInput}
  />})}
   </div>  
     
--- src/OneRecipe.js
+++ src/OneRecipe.js
@@ -1,16 +1,16 @@
-function OneRecipe ({list, newFunc, newFunc1, setList, newFunk1, label, calories, ingredientLines, url, img, ingredients}){
+function OneRecipe ({list, word,newFunk1, newFunc, label, calories, ingredientLines, url, img, ingredients}){
 
     let ArrFromAlIng=""
     let filteredIng=[]
     let filteredArr=[]
 
-
-
-
+//add ingredients in the grossery list
      const filtered=(ing, id)=>{
      filteredArr.push(filteredIng.filter((item, index)=>(index === id)))
-    newFunc(list.push(<li className="list" key={id} onClick={line} >{filteredArr}</li>))
-   }
+     newFunc(list.push(<li className="list" key={id} onClick={line} >{filteredArr.join()}</li>))
+   
+}
+//add and delete ingredients from glossery list
 const line =(e)=>{
     const li= e.target
     li.classList.toggle("red")
@@ -18,19 +18,25 @@ const line =(e)=>{
         li.classList.toggle("del")
     }
 }
+const save=()=>{
+newFunk1(word.push(<div className="saved" key={label}>{label}</div>))
+}
+
 
 
       return(
       <div>
       <div className="eachRecipe">
         <img src={img} alt="alt" width="200px"/>
-<h1>{label}</h1>
+<h1 onClick={save}>{label}</h1>
 <h2>Callories : {calories.toFixed()} Kcal</h2>
 <ul>
     {ingredients.map((ing,id)=>{
          let a= (ArrFromAlIng+=ing.food+"+").split("+")
+         
          if (a.length===ingredients.length+1){filteredIng.push(...a)} 
           return(<li className="allIng" onClick={()=>filtered(ing, id)} key={id}>{ing.food}</li>)
+          
         }
     )}
 </ul>
